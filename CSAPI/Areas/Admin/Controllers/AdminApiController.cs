841e4393f0ef8721d9ba2050de8ded187950a77f
AndronAjai <andronajai32@gmail.com>
2024-08-19 15:19:38 +0530

    Basic Login View

---
+++ CSAPI/Areas/Admin/Controllers/AdminApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSAPI.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Route("api/[area]/[controller]")]
+    [ApiController]
+    public class AdminApiController : Controller
+    {
+        [HttpGet("page1")]
+        public IActionResult Page1()
+        {
+            return View();
+        }
+
+
+        [HttpGet("page2")]
+        public IActionResult Page2()
+        {
+            return View();
+        }
+    }
+}
+
---
+++ CSAPI/Controllers/AccountController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CSAPI.Controllers
+{
+    public class AccountController : Controller
+    {
+
+        private readonly ILogin _loginService;
+
+        public AccountController(ILogin loginService)
+        {
+            _loginService = loginService;
+        }
+
+        public IActionResult Login()
+
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Login(Login model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_loginService.ValidateUser(model.UserName, model.Password, model.Role))
+                {
+                    return Content("User validated");
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Invalid login attempt.");
+                    return Content("Failure");
+                }
+            }
+            return View(model);
+        }
+    }
+}
