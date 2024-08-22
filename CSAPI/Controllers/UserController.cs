528d712bfb772e2f02406c32a84936d5c317d77e
Gauthami-A <gauthami.a02@gmail.com>
2024-08-22 11:01:56 +0530

    Registration Controllerfor user job seeker and employer

--- CSAPI/Controllers/UserController.cs
+++ CSAPI/Controllers/UserController.cs
@@ -13,35 +13,13 @@ namespace CSAPI.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController : ControllerBase
+    public class UserRegistrationController : ControllerBase
     {
         private readonly IUserRepo _repo;
 
-        public UserController(IUserRepo repo)
+        public UserRegistrationController(IUserRepo repo)
         {
             _repo = repo;
-        }
-
-        // GET: api/User
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<ActionResult<IEnumerable<User>>> ShowAll()
-        {
-            var users = await _repo.GetAllAsync();
-            return Ok(users);
-        }
-
-        // GET: api/User/5
-        [HttpGet("{id}")]
-        [AllowAnonymous]
-        public async Task<ActionResult<User>> FindUser(int id)
-        {
-            var user = await _repo.FindByIdAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-            return Ok(user);
         }
 
         // POST: api/User
@@ -57,44 +35,67 @@ namespace CSAPI.Controllers
             return StatusCode((int)HttpStatusCode.Created);
         }
 
-        // PUT: api/User/5
-        [HttpPut("{id}")]
+        // GET: api/User
+        [HttpGet]
         [AllowAnonymous]
-        public async Task<ActionResult> Put(int id, [FromBody] User us)
+        public async Task<ActionResult<IEnumerable<User>>> ShowAll()
         {
-            var user = await _repo.FindByIdAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
-
-            var success = await _repo.UpdateUserAsync(id, us);
-            if (!success)
-            {
-                return BadRequest("Invalid BranchOfficeID.");
-            }
-
-            return NoContent();
+            var users = await _repo.GetAllAsync();
+            return Ok(users);
         }
 
-        // DELETE: api/User/5
-        [HttpDelete("{id}")]
-        [AllowAnonymous]
-        public async Task<ActionResult> Delete(int id)
-        {
-            var user = await _repo.FindByIdAsync(id);
-            if (user == null)
-            {
-                return NotFound();
-            }
 
-            var success = await _repo.DeleteUserAsync(id);
-            if (!success)
-            {
-                return BadRequest("Could not delete the user.");
-            }
+        //// GET: api/User/5
+        //[HttpGet("{id}")]
+        //[AllowAnonymous]
+        //public async Task<ActionResult<User>> FindUser(int id)
+        //{
+        //    var user = await _repo.FindByIdAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return Ok(user);
+        //}
 
-            return NoContent();
-        }
+        //// PUT: api/User/5
+        //[HttpPut("{id}")]
+        //[AllowAnonymous]
+        //public async Task<ActionResult> Put(int id, [FromBody] User us)
+        //{
+        //    var user = await _repo.FindByIdAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var success = await _repo.UpdateUserAsync(id, us);
+        //    if (!success)
+        //    {
+        //        return BadRequest("Invalid BranchOfficeID.");
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //// DELETE: api/User/5
+        //[HttpDelete("{id}")]
+        //[AllowAnonymous]
+        //public async Task<ActionResult> Delete(int id)
+        //{
+        //    var user = await _repo.FindByIdAsync(id);
+        //    if (user == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var success = await _repo.DeleteUserAsync(id);
+        //    if (!success)
+        //    {
+        //        return BadRequest("Could not delete the user.");
+        //    }
+
+        //    return NoContent();
+        //}
     }
 }
